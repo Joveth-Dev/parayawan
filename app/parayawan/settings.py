05afd493637a8176a070a6b6b8a82dd1125dcf96
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-01-25 17:06:34 +0800

    Add default permission classes to REST framework settings

--- app/parayawan/settings.py
+++ app/parayawan/settings.py
@@ -135,4 +135,7 @@ REST_FRAMEWORK = {
         # "rest_framework.renderers.StaticHTMLRenderer",
         "rest_framework.renderers.TemplateHTMLRenderer",
     ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
