9b694b984307a3e67fd56015a0403c4bbead76dd
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-01-25 17:01:40 +0800

    Add user management functionality with serializers, views, and templates

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'users': serializer.data}, template_name="core/users_list.html")
